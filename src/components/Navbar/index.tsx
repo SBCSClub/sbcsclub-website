2f757a0ca641e891a5786abb6a6edcef6628a0c5
Mahit Mehta <mahit.py@gmail.com>
2022-10-19 01:29:29 -0400

    Stability Update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -22,8 +22,9 @@ const Navbar = () => {
             style={{
                 background: minimized ? "rgba(0,0,0, 0.5)" : "transparent",
                 backdropFilter: minimized ? "blur(5px)" : undefined,
+                WebkitBackdropFilter: minimized ? "blur(5px)" : undefined
             }}
-            className="fixed p-3 w-full z-50 transition-all">
+            className="fixed px-3 py-2 w-full z-50 transition-all">
             <nav className="flex justify-center items-center">
                 <div style={{ 
                     height: minimized ? 50 :  100, width: minimized ? 50 :  100 
